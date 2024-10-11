de64f88ab2e19104d21206cff7e22bae7d71f52f
Hajar <hajarayaddouz29@gmail.com>
2024-10-11 16:57:32 +0200

    we can list employees next to the compagny they're assign to

--- src/Entity/Employe.php
+++ src/Entity/Employe.php
@@ -97,6 +97,8 @@ class Employe
 
     public function __toString()
     {
-        return $this->prenom." ".$this->nom;
+        return $this->prenom." ".$this->nom." ".$this->getEntreprise()->getRaisonSociale();
     }
+
+
 }
